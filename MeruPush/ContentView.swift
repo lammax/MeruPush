e76b79d2c7f6fae1fc78f0c605fc9bbe8a250bad
Lammax <lammax@mail.ru>
2020-08-20 20:33:31 +0300

    Initial Commit

---
+++ MeruPush/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeruPush
+//
+//  Created by Mac on 20.08.2020.
+//  Copyright © 2020 Lammax. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
